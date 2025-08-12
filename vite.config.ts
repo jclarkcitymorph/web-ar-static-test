669f734110ca97f13fb143cfde4a1c2d33c624bb
Jclark <justindanielclark@gmail.com>
2025-08-11 18:03:18 -0700

    + attempt to make nft work

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ command }) => ({
   // Use repo name for builds (gh-pages), root for dev (localhost)
-  base: command === "build" ? "/web-ar-static-test/" : "/",
+  base: command === "build" ? "web-ar-static-test/" : "/",
   plugins: [tailwindcss()],
 }));
